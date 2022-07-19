35535faa21c689034ea8f94d6f13bd5e1e4eff21
luis-c-v <lcanovaz@bowdoin.edu>
2022-07-19 17:29:25 -0600

    update to sounds

--- gd-trends/src/App.js
+++ gd-trends/src/App.js
@@ -1,17 +1,18 @@
-import logo from './logo.svg';
+//import logo from './logo.svg';
 import './App.css';
-import Header from './components/header/Header';
-import Search from './components/search/Search';
+//import Header from './components/header/Header';
+//import Search from './components/search/Search';
 //import Videos from './components/videos.js';
-import Hashtags from './components/hashtags.js';
-//import Sounds from './components/sounds.js';
+//import Hashtags from './components/hashtags.js';
+import Sounds from './components/sounds.js';
 
 function App() {
   return (
     <>
-    <Header/>
+    {/* <Header/>
     <Search/>
-    <Hashtags/>
+    <Hashtags/> */}
+    <Sounds/>
     </>
   );
 }
--- gd-trends/src/components/sounds.js
+++ gd-trends/src/components/sounds.js
@@ -2,6 +2,9 @@ import React from 'react';
 import '../styles/sounds.css';
 
 // const sounds = []
+function playSound(songUrl) {
+    new Audio(songUrl).play()
+};
 
 const Sounds = () => {
     return (
@@ -9,8 +12,17 @@ const Sounds = () => {
             <div className='sound-container'>
                 <div className='title'>Popular Sounds</div>
                 <div className='grid-container'>
+                    {/* {hashtags.map((tag) => (
+                        <tr><td>{tag.name}</td></tr>
+                    ))} */}
+                    {/* {sounds.map((sound) => (
+                        <div className='sound' onclick='playSound(sounds.url)>Play</div>
+                    ))} */}
+
                     <div className='sound' style={{"background-color": 'blue'}}>
-                        play1
+                        <button className='play' onclick='playSound(sounds.url)'>
+                            <span class='icon-ic_global_play_dark mr-1'>play1</span>
+                        </button>
                     </div>
                     <div className='sound' style={{"background-color": 'red'}}>
                         play2
@@ -20,6 +32,18 @@ const Sounds = () => {
                     </div>
                     <div className='sound' style={{"background-color": 'yellow'}}>
                         play4
+                    </div>
+                    <div className='sound' style={{"background-color": 'purple'}}>
+                        play5
+                    </div>
+                    <div className='sound' style={{"background-color": 'orange'}}>
+                        play6
+                    </div>
+                    <div className='sound' style={{"background-color": 'pink'}}>
+                        play7
+                    </div>
+                    <div className='sound' style={{"background-color": 'brown'}}>
+                        play8
                     </div>
                 </div>
             </div>
